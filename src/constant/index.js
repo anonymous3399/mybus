44517fdb1272f080dca478ec35df77140d76ff07
anonymous3399 <meet@algomage.com>
2023-12-27 23:30:21 +0530

    added error message constant

---
+++ src/constant/index.js
@@ -0,0 +1,8 @@
+exports.MESSAGE_401 = "Please ensure you are logged in";
+exports.MESSAGE_403 =
+  "Access Denied. Please ensure you are logged in with valid credentials";
+exports.INTERNAL_403_MESSAGE = "Forbidden";
+exports.INTERNAL_401_MESSAGE = "Unauthenticated";
+exports.INTERNAL_400_MISSING_PARAMS_MESSAGE = "Parameter missing";
+exports.MESSAGE_400_MISSING_PARAMS = "parameter is missing";
+exports.MESSAGE_500_INTERNAL_SERVER_ERROR = "Internal server error";
