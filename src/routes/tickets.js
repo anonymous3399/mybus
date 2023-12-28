5f88406eaa2362ea27791c4369506a2003c1d06f
anonymous3399 <meet@algomage.com>
2023-12-28 14:27:23 +0530

    change path

--- src/routes/tickets.js
+++ src/routes/tickets.js
@@ -11,6 +11,6 @@ ticketRouter.get("/open-bookings", getOpenBooking);
 
 ticketRouter.get("/user-details/:id" , getUserDetails)
 
-ticketRouter.post("/user-details/:id" , postUserDetails)
+ticketRouter.post("/user-details" , postUserDetails)
 
 module.exports = ticketRouter;
