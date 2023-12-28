9cf8c01412e5d80234d77dfb3195028b3bb7e319
anonymous3399 <meet@algomage.com>
2023-12-28 09:09:46 +0530

    added ticket status

--- src/routes/tickets.js
+++ src/routes/tickets.js
@@ -1,12 +1,11 @@
 const { Router } = require("express");
-const { singleTicketStatus } = require("../controller/ticket");
-const { getTicketStatus } = require("../services/ticket");
+const { singleTicketStatus, getTicketStatus } = require("../controller/ticket");
 
 const ticketRouter = Router();
 
 ticketRouter.get("/get-status/:id", singleTicketStatus);
 
-ticketRouter.get("/status", () => {});
+ticketRouter.get("/status", getTicketStatus);
 
 ticketRouter.get("/url", () => {});
 
